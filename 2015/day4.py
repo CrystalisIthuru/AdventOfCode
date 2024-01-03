ff44c6a3f90e05b26cd60863b90207c8ffdd0ae0
Andrew Carter <steven.a.carter49.ctr@army.mil>
2024-01-03 13:25:06 -0600

    2015 Day 4

---
+++ 2015/day4.py
@@ -0,0 +1,20 @@
+import hashlib
+
+def find_secret_key_hash(secret_key, required_zeros = 5):
+    
+    i = 0
+    while True:
+        
+        key = secret_key + str(i)
+        md5 = hashlib.md5(key.encode("utf-8")).hexdigest()
+        
+        if all([char == "0" for char in md5[:required_zeros]]):
+            return i
+        i += 1
+    
+if __name__ == "__main__":
+    
+    secret_key = "iwrupvqb"
+    print(find_secret_key_hash(secret_key))
+    print(find_secret_key_hash(secret_key, 6))
+    
